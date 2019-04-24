a7b56e127378466426c1a1ec314d1ebfdd31eee6
aasdf <aasdf>
2019-04-24 20:14:09 +0530

    done testing as well

--- Assignment3/driver.py
+++ Assignment3/driver.py
@@ -2,6 +2,7 @@ import hard_code_params
 import pickle
 import numpy as np
 from layer import Layer
+from sklearn.metrics import precision_recall_fscore_support
 
 def read_data():
     with open(hard_code_params.path_pickle_train,'rb') as file_handle:
@@ -12,8 +13,9 @@ def read_data():
     with open(hard_code_params.path_pickle_test,'rb') as file_handle:
         test_dict = pickle.load(file_handle)
         test = test_dict['test']
+        test_labels = test_dict['labels']
 
-    return train,labels,test
+    return train,labels,test,test_labels
 
 # def sanitycheck(weights_list,train,labels):
 #     num_output = len ( set(labels) )
@@ -43,9 +45,10 @@ def main():
         hidden_units = (x,y)
         weights_list.append(Layer(hidden_units))
 
-    train,labels,test = read_data()
-    
+    train,labels,test,test_labels = read_data()
+
     labels = np.array([0 if x == 'cat' else 1 for x in labels])
+    test_labels = np.array([0 if x == 'cat' else 1 for x in test_labels])
     # print(labels)
     
     for epoch in range(hard_code_params.epochs):
@@ -65,7 +68,7 @@ def main():
             layer.update()
     
 
-    input_for_layers = train
+    input_for_layers = test
     for layer in weights_list:    
         input_for_layers = layer.forward(input_for_layers)
 
@@ -77,14 +80,15 @@ def main():
 
 
     ## TODO: CHANGE THE INDICES FOR TESTING ACCORDINGLY
-    for train_point_index in range(labels.shape[0]):
-        if input_for_layers[train_point_index] == labels[train_point_index]:
-            print(train_point_index,input_for_layers[train_point_index],labels[train_point_index])
+    for test_point_index in range(test_labels.shape[0]):
+        if input_for_layers[test_point_index] == test_labels[test_point_index]:
+            # print(train_point_index,input_for_layers[train_point_index],labels[train_point_index])
             accuracy += 1
     # print(train)
-    print('accuracy: ',accuracy/train.shape[1])
 
+    print('accuracy: ',accuracy/test.shape[1],' num cats: ',test_labels[test_labels == 0].shape,' num dogs: ',test_labels[test_labels == 0].shape)
     # sanitycheck(weights_list,train,labels)
+    print(precision_recall_fscore_support(test_labels,input_for_layers))
 
 if __name__ == '__main__':
     main()
--- Assignment3/preproc.py
+++ Assignment3/preproc.py
@@ -14,33 +14,41 @@ def reduce_data():
         os.mkdir('./reduced/')
         os.mkdir('./reduced/train/')
         os.mkdir('./reduced/test/')
-        testfiles = [f for f in listdir(hard_code_params.path_test) if isfile(join(hard_code_params.path_test, f))][:500]
-        trainfiles = [f for f in listdir(hard_code_params.path_train) if isfile(join(hard_code_params.path_train, f))][:500]
+        testfiles = [f for f in listdir(hard_code_params.path_train) if isfile(join(hard_code_params.path_train, f))][10000:15000]
+        # testfiles = [f for f in listdir(hard_code_params.path_test) if isfile(join(hard_code_params.path_test, f))][10000:15000]
+        trainfiles = [f for f in listdir(hard_code_params.path_train) if isfile(join(hard_code_params.path_train, f))][:10000]
+        
+        
         testing_data = np.zeros((hard_code_params.dim*hard_code_params.dim,1))
         data_dict = {}
         labels = []
         print('testing_data')
-        print('max for test',len(testfiles))
+        # print('max for test',len(testfiles))
+  
+        testing_data = np.zeros((hard_code_params.dim*hard_code_params.dim,1))
         for testfile in testfiles:
-            
-            img = PIL.Image.open(hard_code_params.path_test+'/'+testfile).convert('L')
+            labels += [testfile.split(hard_code_params.delim)[0]]
+            # img = PIL.Image.open(hard_code_params.path_test+'/'+testfile).convert('L')
+            img = PIL.Image.open(hard_code_params.path_train+'/'+testfile).convert('L')
             img = img.resize((hard_code_params.dim,hard_code_params.dim))
             # img.save('test.png')
             # print(img.size)
             pix = np.reshape(np.array(img).flatten(),(hard_code_params.dim*hard_code_params.dim,1))
             testing_data = np.hstack( (testing_data,pix) )
-            print(testing_data.shape)
         testing_data = testing_data[:,1:]
+        print(testing_data.shape)
         # print(testing_data.shape)
         testing_data =testing_data.T
         testing_data = (testing_data - np.mean(testing_data,axis=0))/np.std(testing_data,axis=0)
         testing_data =testing_data.T
         data_dict['test'] = testing_data
+        data_dict['labels'] = labels
         with open(hard_code_params.path_pickle_test,'wb') as f:
             pickle.dump(data_dict,f)
         
         training_data = np.zeros((hard_code_params.dim*hard_code_params.dim,1))
         data_dict = {}
+        labels = []
         print('training_data')
         print('max in train is ',len(trainfiles))
         for trainfile in trainfiles:
@@ -51,8 +59,8 @@ def reduce_data():
             # print(img.size)
             pix = np.reshape(np.array(img).flatten(),(hard_code_params.dim*hard_code_params.dim,1))
             training_data = np.hstack((training_data,pix))
-            print(training_data.shape)
         training_data = training_data[:,1:]
+        print(training_data.shape)
         training_data = training_data.T
         training_data = (training_data - np.mean(training_data,axis=0))/np.std(training_data,axis=0)
         training_data = training_data.T
